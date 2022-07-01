952ec13661132cc7d99e60b5f6c46e98c6a69cbd
jeremias <jeremiasprogrammer@gmail.com>
2022-07-01 20:56:09 -0300

    finish readme and add files resource

--- README.md
+++ README.md
@@ -3,35 +3,18 @@
 
 ## Para utiliza-lo é necessário ter uma máquina UNIX (linux ou mac) e os seguintes arquivos:
 
-### - Arquivo que
-### - Arquivo que 
+### - Arquivo commands.txt com todos os comandos que serão executados pelo algoritmo
+### - Arquivo customer.txt com todos os consumidores de recursos.
 
 
 ## Como executar:
 
 ### - Existem 3 parametros extremamente necessários para execução. 
 
-### - Exemplo: 
+### - Exemplo: ./banker 10 5 7
 
 ### - Utilizar o seguinte comando para limpar o arquivo executável gerado: make clean
 
 ### - Utilizar o seguinte comando para gerar um novo arquivo executável: make
 
 ### - A execução dele não funciona no make por conta dos parametros que devem ser passados, utilize o exemplo mostrado.
-
-
-## Implementações:
-
-### - 
-### - 
-
-### - 
-
-### - 
-
-### - 
-
-### - 
-
-### - 
-
--- dev/main.c
+++ dev/main.c
@@ -7,17 +7,68 @@
 #define DEFAULT_ERR_MSG "\nOcorreu um problema: "
 #define CLOSE_MSG "PROGRAMA ENCERRADO\n"
 
+//Path files CONSTANTES
+#define PATH_CUSTOMER "customers.txt"
+#define PATH_COMMANDS "commands.txt"
+#define PATH_RESULT "result.txt"
+
+// Files POINTERS
+FILE *customers_file;
+FILE *commands_file;
+FILE *result_file;
+FILE *temp_file;
+
 void print_err(char *err);
+void get_file_error();
+void prepare_to_write_result();
 
 
 int main(int argc, char **argv) {
     printf("Dev area operating...\n");
 
+    //Verify initial erros
+    get_file_error(PATH_CUSTOMER);
+    get_file_error(PATH_COMMANDS);
+
+
+    fclose(result_file)
+
     return 0;
 }
 
-void print_err(char *err){
+void print_err(char *err) {
     printf("%s\n", err);
     printf(CLOSE_MSG);
     exit(1);
+}
+
+void get_file_error(char *file_name) {
+    char *temp[3];
+    *temp = &file_name[strlen(file_name)-3];
+
+    if(strcmp(*temp, "txt") != 0){
+        char *ERR = DEFAULT_ERR_MSG"O arquivo passado não é um arquivo .txt";
+        print_err(ERR);
+        exit(1);
+    }
+
+    temp_file = fopen(file_name, "r");
+
+    if (temp_file == NULL){
+        char *ERR = DEFAULT_ERR_MSG"Ao abrir o arquivo. Está vazio ou não existe";
+        print_err(ERR);
+        exit(1);
+    }
+
+    fclose(temp_file);
+}
+
+void prepare_to_write_result(){
+    //Resetando o arquivo a primeira vez que o programa é executado
+    result_file = fopen(PATH_RESULT, "w");
+
+    fclose(result_file);
+
+    //Abrindo arquivo para append no arquivo
+    result_file = fopen(PATH_RESULT, "a");
 }
